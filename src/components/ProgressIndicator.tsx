a649a2bcb6d43ce9e4c4690fdd31704b3a318bc5
BTC415 <alenschmitt415@gmail.com>
2024-08-28 21:30:45 -0700

    Some styling issues on ProgressIndicator comps

--- src/components/ProgressIndicator.tsx
+++ src/components/ProgressIndicator.tsx
@@ -1,7 +1,7 @@
 import ProgressPane from "./ProgressPane";
 
 export const TMCheckerElement = () => {
-  
+
   return (
     <div id="page-header" className="flex gap-2 content-center">
       <h3 className='font-mont leading-[28px] text-[18px]'>Trade Mark Today</h3>
@@ -14,6 +14,7 @@ const ProgressIndicator = ({ stage }: { stage: number }) => {
   return (
     <section id="progressIndicator">
       <TMCheckerElement />
+      
       <div id="steps-container" className="flex flex-col md:flex-row justify-between items-start gap-x-6 bg-white rounded-xl pt-4 shadow-[0_0_8px_8px_#fff]">
         <ProgressPane className={`${stage >= 1 ? "state-active" : ""}`} title="Getting started" />
         <ProgressPane className={`${stage >= 2 ? "state-active" : ""}`} title="Choose trade mark" />
