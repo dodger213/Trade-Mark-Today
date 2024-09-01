9e903042897b99cafb37fc0d165a6e90848a2b07
dodger213 <mitsurukudo213@gmail.com>
2024-09-01 18:36:13 -0500

    Changed the styling of TM check layout
    Co-authored-by: dodger213 <mitsurukudo213@gmail.com>
    Co-authored-by: BTC415 <alenschmitt415@gmail.com>

--- src/layout/TMCheckLayout.tsx
+++ src/layout/TMCheckLayout.tsx
@@ -4,14 +4,15 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 
 const TMCheckLayout = (page: ReactElement, pageProps: AppProps) => {
-   return (
-        <div id="main-start-section" className='flex flex-col pt-20'>
-            <div className='pb-5 flex-grow'>
-                <Header />
-                {page}
-            </div>
-            <Footer />
-        </div>
-    )
+  return (
+    <div id="main-start-section" className='flex flex-col pt-20'>
+      <div className='pb-5 flex-grow'>
+        <Header />
+        {page}
+      </div>
+      <Footer />
+    </div>
+  )
 }
-export default TMCheckLayout;
+
+export default TMCheckLayout
